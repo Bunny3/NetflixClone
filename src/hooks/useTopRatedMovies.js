c31b19c0cecbf4ae551abe585408a8704d268127
Shubham Agarwal <shubham.sa.agarwal@oracle.com>
2024-03-30 16:37:05 +0530

    Different Movie List

---
+++ src/hooks/useTopRatedMovies.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { API_OPTION } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addTopRatedMovies } from "../utils/movieSlice";
+
+const useTopRatedMovies = () => {
+
+  const dispatch = useDispatch();
+  const getTopRatedMovies = async() => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+      API_OPTION
+    );
+    const json = await data.json();
+    dispatch(addTopRatedMovies(json.results));
+  };
+
+  useEffect(()=>{
+    getTopRatedMovies();
+  },[]);
+}
+
+export default useTopRatedMovies;
