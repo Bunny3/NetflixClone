fc2c540189a7d99b4ce44f005cde4e1995509c90
Shubham Agarwal <shubham.sa.agarwal@oracle.com>
2024-03-31 21:46:44 +0530

    Adding Audio

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -11,7 +11,7 @@ const VideoBackground = ({ movieId }) => {
       
       <iframe 
       className='w-screen aspect-video'
-      src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?autoplay=1&mute=1&loop=3;playlist="+trailerVideo?.key} 
+      src={"https://www.youtube.com/embed/"+trailerVideo?.key+"?autoplay=1&loop=3;playlist="+trailerVideo?.key} 
       title="YouTube video player" 
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       referrerPolicy="strict-origin-when-cross-origin" 
